7948f7284d94afad9a5b55f6d974d492770a92f4
Yoan Gabriele <=yoan.gabriele@gmail.com>
2020-06-05 13:43:04 +0200

    Adding listen port

--- coreapp.js
+++ coreapp.js
@@ -2,13 +2,46 @@ const express = require ('express');
 const coreapp = express();
 const port = 3000;
 
+
+coreapp.listen(port,function(){
+
+    console.log('Backend Server has started at http://localhost:'+port);
+
+});
+
+//Respond to POST request on the root route (/) on the coreapp homepage
+
+coreapp.post('/',function(req,res){
+
+    res.send('Got a POST request');
+});
+
+
+
+//Respond with Hello World on the homepage
+
 coreapp.get('/',function(req,res){
 
     res.send('Hello world');
 });
 
-coreapp.listen(3000,function(){
 
-    console.log('Backend Server started at port 3000');
 
+
+//Respond to a PUT request on the route  user (/user) on the coreapp
+
+coreapp.put('/user',function(req,res){
+
+    res.send('Got a PUT request at /user');
 });
+
+
+
+//Respond to a DELETE request on the route  user (/user) on the coreapp 
+
+coreapp.delete('/user',function(req,res){
+
+    res.send('Got a DELETE request at /user');
+});
+
+
